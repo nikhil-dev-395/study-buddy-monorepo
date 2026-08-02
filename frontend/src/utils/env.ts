9c084d465440dba3f43c30099d70efecca290808
Nikhil Wankhade <nikhilw395@gmail.com>
2026-08-02 18:24:08 +0530

    fix: node env eg removed

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 export const envVariables = z.object({
   VITE_BASE_URL: z.string().url().startsWith("http://", "https://"),
   VITE_API_BASE_URL: z.string().url().startsWith("http://", "https://"),
-  VITE_NODE_ENV: z.enum(["development", "production"]),
+  VITE_NODE_ENV: z.string().refine((val) => ["development", "production"].includes(val), {
+    message: "VITE_NODE_ENV must be either 'development' or 'production'",
+  }),
   VITE_GOOGLE_CLIENT_ID: z.string(),
   VITE_GOOGLE_CLIENT_SECRET: z.string(),
 });
