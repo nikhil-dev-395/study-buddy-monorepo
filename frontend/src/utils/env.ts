a408435b210d7b8dbbe662a8ccba93c27245458b
Nikhil Wankhade <nikhilw395@gmail.com>
2026-08-02 18:26:49 +0530

    fix(frontend): node env suggestion removed

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -3,9 +3,7 @@ import { z } from "zod";
 export const envVariables = z.object({
   VITE_BASE_URL: z.string().url().startsWith("http://", "https://"),
   VITE_API_BASE_URL: z.string().url().startsWith("http://", "https://"),
-  VITE_NODE_ENV: z.string().refine((val) => ["development", "production"].includes(val), {
-    message: "VITE_NODE_ENV must be either 'development' or 'production'",
-  }),
+  VITE_NODE_ENV: z.string(),
   VITE_GOOGLE_CLIENT_ID: z.string(),
   VITE_GOOGLE_CLIENT_SECRET: z.string(),
 });
